eecf5a142d4d3151419c0d00fe338c782315b133
dqliangjun <dqliangjun@126.com>
2018-11-15 17:41:11 +0800

    * 假如了分包工具

--- baselib/src/main/java/net/lll0/base/BaseApplication.java
+++ baselib/src/main/java/net/lll0/base/BaseApplication.java
@@ -1,11 +1,12 @@
 package net.lll0.base;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by liangjun on 2018/11/13
  */
-public class BaseApplication extends Application {
+public class BaseApplication extends MultiDexApplication {
     public static  BaseApplication application;
 
 
